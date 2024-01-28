12568aa346aa456ffef22008825cf04b27130a56
Artur Leleiko <artur_98@ukr.net>
2024-01-28 18:35:52 +0200

    Create models: AirplaneType, Airplane, Airport, Route, Crew, Flight

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,90 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=125)
+    closest_big_city = models.CharField(max_length=125)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="destination_routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"Route: {self.source.name} - {self.destination.name}"
+
+    class Meta:
+        ordering = ["distance"]
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane, on_delete=models.CASCADE, related_name="flights"
+    )
+    crew = models.ManyToManyField(Crew, blank=True, related_name="flights")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return str(self.route) + " " + str(self.departure_time)
+
+    class Meta:
+        ordering = ["-departure_time"]
