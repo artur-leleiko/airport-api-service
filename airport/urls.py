f0dd3791407eccd2f70aa76a30434dbd582fe495
Artur Leleiko <artur_98@ukr.net>
2024-01-28 19:19:45 +0200

    Create url for each view

---
+++ airport/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    FlightViewSet
+)
+
+
+router = routers.DefaultRouter()
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("flights", FlightViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "airport"
