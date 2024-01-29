4a901275e83236572c4874f5253c5108121024a1
Artur Leleiko <artur_98@ukr.net>
2024-01-29 21:20:54 +0200

    Add filter by source + destination for Route

--- airport/views.py
+++ airport/views.py
@@ -55,6 +55,24 @@ class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.select_related("source", "destination")
     serializer_class = RouteSerializer
 
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
+        if source:
+            queryset = queryset.filter(
+                source__closest_big_city__icontains=source
+            )
+
+        if destination:
+            queryset = queryset.filter(
+                destination__closest_big_city__icontains=destination
+            )
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return RouteListSerializer
