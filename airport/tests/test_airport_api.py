0aa1dcb0efd6e8ddca5daac0165e8492f5e7f8e2
Artur Leleiko <artur_98@ukr.net>
2024-02-02 12:42:54 +0200

    Add tests for AirplaneType, Airplane, Airport

---
+++ airport/tests/test_airport_api.py
@@ -0,0 +1,90 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from airport.models import Airport
+from airport.serializers import AirportSerializer
+
+AIRPORT_URL = reverse("airport:airport-list")
+
+
+def sample_airport(**params):
+    defaults = {
+        "name": "Test",
+        "closest_big_city": "Kyiv",
+    }
+    defaults.update(params)
+
+    return Airport.objects.create(**defaults)
+
+
+class UnauthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(AIRPORT_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_airport(self):
+        sample_airport()
+
+        res = self.client.get(AIRPORT_URL)
+
+        airports = Airport.objects.order_by("id")
+        serializer = AirportSerializer(airports, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_filter_airports_by_closest_big_city(self):
+        airport1 = sample_airport(name="airport1", closest_big_city="Rome")
+        airport2 = sample_airport(name="airport2", closest_big_city="London")
+
+        response = self.client.get(AIRPORT_URL, {"city": "rome"})
+
+        serializer1 = AirportSerializer(airport1)
+        serializer2 = AirportSerializer(airport2)
+
+        self.assertIn(serializer1.data, response.data)
+        self.assertNotIn(serializer2.data, response.data)
+
+    def test_create_airport_forbidden(self):
+        payload = {
+            "name": "Test",
+            "closest_big_city": "Kyiv",
+        }
+        res = self.client.post(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com", "testpass", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_airport(self):
+        payload = {
+            "name": "Test",
+            "closest_big_city": "Kyiv",
+        }
+        res = self.client.post(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
